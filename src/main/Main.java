1af77ba6f83488c36a0f3b2d2f4d17c8ef59e21b
kristoph <krzys.maciejewski@gmail.com>
2019-04-01 19:08:02 +0200

    Klasa Entity

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,17 @@ package main;
 
 public class Main {
 
+    private static final int WINDOW_WIDTH = 384;
+    private static final int WINDOW_HEIGHT = 240;
+
     public static void main(String[] args) {
 
         GameEngine game;
+
         if (args.length == 0) {
-            game = new GameEngine(384, 240, 2);
+            game = new GameEngine(WINDOW_WIDTH, WINDOW_HEIGHT, 2);
         } else if (args.length == 1) {
-            game = new GameEngine(384, 240, Integer.parseInt(args[0]));
+            game = new GameEngine(WINDOW_WIDTH, WINDOW_HEIGHT, Integer.parseInt(args[0]));
         } else {
             game = new GameEngine(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
         }
---
+++ src/main/entities/Entity.java
@@ -0,0 +1,83 @@
+package main.entities;
+
+import main.GameEngine;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Entity {
+
+    private BufferedImage texture;
+    private double x, y;
+
+    public void draw(Graphics g) {
+        g.drawImage(texture, ((int) x - GameEngine.currentMap.getOffsetX()) * GameEngine.scale
+                , ((int) y) * GameEngine.scale, texture.getWidth() * GameEngine.scale
+                , texture.getHeight() * GameEngine.scale, null);
+    }
+
+    // 0 - góra, 1 - prawo, 2 - dół, 3 - lewo
+    boolean checkMapCollision(int dir, double vx, double vy) {
+        switch (dir) {
+            case 0:
+                if (GameEngine.currentMap.getTileByXY(Math.floor(getX())
+                        , Math.floor(getY()) + vy).isSolid()
+                        || GameEngine.currentMap.getTileByXY(Math.floor(getX()) - 1 + getTexture().getWidth()
+                        , getY() + vy).isSolid()) {
+                    return true;
+                }
+                break;
+            case 1:
+                if (GameEngine.currentMap.getTileByXY(Math.floor(getX()) + vx + getTexture().getWidth()
+                        , getY()).isSolid()
+                        || GameEngine.currentMap.getTileByXY(Math.floor(getX()) + vx + getTexture().getWidth()
+                        , Math.floor(getY()) - 2 + getTexture().getHeight()).isSolid()) {
+                    return true;
+                }
+                break;
+            case 2:
+                if (GameEngine.currentMap.getTileByXY(Math.floor(getX())
+                        , (getY() + vy) + getTexture().getHeight() - 1).isSolid()
+                        || GameEngine.currentMap.getTileByXY(Math.floor(getX()) - 1 + getTexture().getWidth()
+                        , (getY() + vy) + getTexture().getHeight() - 1).isSolid()) {
+                    return true;
+                }
+                break;
+            case 3:
+                if (GameEngine.currentMap.getTileByXY(Math.floor(getX()) + vx
+                        , getY()).isSolid()
+                        || GameEngine.currentMap.getTileByXY(Math.floor(getX()) + vx
+                        , Math.floor(getY()) - 2 + getTexture().getHeight()).isSolid()) {
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
+    void setTexture(BufferedImage texture) {
+        this.texture = texture;
+    }
+
+    BufferedImage getTexture() {
+        return texture;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    double getY() {
+        return y;
+    }
+
+    void setX(double x) {
+        this.x = x;
+    }
+
+    void setY(double y) {
+        this.y = y;
+    }
+
+}
+

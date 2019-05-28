8c499e71c1b6addef48724f0e94bb3ca4c1c01cc
Powerofnothing <48891563+Powerofnothing@users.noreply.github.com>
2019-05-28 17:58:11 +0200

    Test getFromList
    Test getFromList

---
+++ src/main/entities/Test_Portals_getFromList.java
@@ -0,0 +1,21 @@
+ackage main.entities;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PortalsTest {
+
+    @Test
+    Portals portal = new Portals();
+    portal.init();    
+    Assert.assertEquals(Portal.getFromList(0), portal);
+    Assert.assertNull(Portal.getFromList(2));
+    
+    
+    
+    
+    
+    }
+
+private static ArrayList<Portal> portalsList;

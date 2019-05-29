f463d91742df687163c3aba5524a80583a6bf0ca
Powerofnothing <48891563+Powerofnothing@users.noreply.github.com>
2019-05-29 12:09:38 +0200

    Update Test_Portals_init.java

--- src/main/entities/Test_Portals_init.java
+++ src/main/entities/Test_Portals_init.java
@@ -11,9 +11,10 @@ public class PortalsTest {
         Portals portal = new Portals();
         Portals.init();
 
-        Assert.assertEquals(Portals.portal(0).getId(), 0);
-        Assert.assertEquals(Portals.getFromList(id).getX(), 560);
-        Assert.assertEquals(Portals.getFromList(id).isAnimated(), 1);
-        Assert.assertNull(Portals.portal(2));
+        Assert.assertEquals(Portals.getFromList(0).getId(), 0);
+        Assert.assertEquals(Portals.getFromList(0).getX(), 560);
+        Assert.assertEquals(Portals.getFromList(0).isAnimated(), 1);
+        Assert.assertNull(Portals.getFromList(2));
 
     }
+}
